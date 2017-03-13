85ea2d9ee92f4cf02d883755d3473df35122ebe6
DESKTOP-9CKSS1Q\CAMT <natrada_chairat@cmu.ac.th>
2017-03-13 17:58:52 +0700

    implement when target is USD and source is USD

--- camt234/src/main/java/ultility/Currency.java
+++ camt234/src/main/java/ultility/Currency.java
@@ -13,7 +13,46 @@ public class Currency {
             if (sourceCurrrency.equalsIgnoreCase("THB")) {
                 exchangeCurrency = (amount / 33) * 1.01;
             } else exchangeCurrency = (amount / 1.01) * 33;
-        }
+        }else if (sourceCurrrency.equalsIgnoreCase("USD") && targetCurrency.equalsIgnoreCase("THB")) {
+            exchangeCurrency = amount * 33;
+
+            // target currency is EUR
+        } else if (sourceCurrrency.equalsIgnoreCase("USD") && targetCurrency.equalsIgnoreCase("EUR"))
+            exchangeCurrency = amount * 1.01;
+
+            // source currency is THB
+        else if (sourceCurrrency.equalsIgnoreCase("THB") && (targetCurrency.equalsIgnoreCase("USD")))
+            exchangeCurrency = amount / 33;
+
+            // target currency is THB
+        else if (sourceCurrrency.equalsIgnoreCase("USD") && targetCurrency.equalsIgnoreCase("THB")) {
+            exchangeCurrency = amount * 33;
+
+            // target currency is EUR
+        } else if (sourceCurrrency.equalsIgnoreCase("USD") && targetCurrency.equalsIgnoreCase("EUR"))
+            exchangeCurrency = amount * 1.01;
+
+            // source currency is THB
+
+        else if (sourceCurrrency.equalsIgnoreCase("THB") && (targetCurrency.equalsIgnoreCase("USD")))
+            exchangeCurrency = amount / 33;
+
+            // target currency is THB
+        else if (sourceCurrrency.equalsIgnoreCase("USD") && targetCurrency.equalsIgnoreCase("THB")) {
+            exchangeCurrency = amount * 33;
+
+        } else if (sourceCurrrency.equalsIgnoreCase("USD") && targetCurrency.equalsIgnoreCase("EUR"))
+            exchangeCurrency = amount * 1.01;
+
+            // source currency is THB
+
+        else if (sourceCurrrency.equalsIgnoreCase("THB") && (targetCurrency.equalsIgnoreCase("USD"))) {
+            exchangeCurrency = amount / 33;
+
+            // source currency is EUR
+        } else if (sourceCurrrency.equalsIgnoreCase("EUR") && (targetCurrency.equalsIgnoreCase("USD")))
+            exchangeCurrency = amount / 1.01;
+        else exchangeCurrency=Double.NaN;
         return exchangeCurrency;
 
     }
