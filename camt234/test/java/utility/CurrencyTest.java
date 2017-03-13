350c5b6cf226e8ecf2c7a74681483aceef531944
DESKTOP-9CKSS1Q\CAMT <kritsanah_supphasri@cmu.ac.th>
2017-03-13 17:24:15 +0700

    A  create test case

--- camt234/test/java/utility/CurrencyTest.java
+++ camt234/test/java/utility/CurrencyTest.java
@@ -12,12 +12,14 @@ import static org.hamcrest.number.IsCloseTo.closeTo;
 public class CurrencyTest {
     @Test
     public void testExchange() {
+        Currency currency = new Currency();
         //the double equal is check for the third digit precision (the second
         // paramether)).you may fix the second parameter
         //the first parameter of the closeTo is the value which will be check
         // the return value of the exchange method
 
         assertThat(currency.exchange("USD", 1.0, "USD"), closeTo(1.0, 0.001));
+        assertThat(currency.exchange("USD", 33.0, "THB"), closeTo(1.0, 0.001));
     }
 
 }
