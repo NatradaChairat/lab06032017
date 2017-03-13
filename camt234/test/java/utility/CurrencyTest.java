6202c25e1432ce269fa37887addc479171d509fb
DESKTOP-9CKSS1Q\CAMT <natrada_chairat@cmu.ac.th>
2017-03-13 17:46:46 +0700

    add the test case

--- camt234/test/java/utility/CurrencyTest.java
+++ camt234/test/java/utility/CurrencyTest.java
@@ -19,7 +19,8 @@ public class CurrencyTest {
         // the return value of the exchange method
 
         assertThat(currency.exchange("USD", 1.0, "USD"), closeTo(1.0, 0.001));
-        //assertThat(currency.exchange("USD", 33.0, "THB"), closeTo(1.0, 0.001));
+        assertThat(currency.exchange("USD", 10.0, "THB"), closeTo(330.0, 0.001));
+        assertThat(currency.exchange("USD", 10.0, "EUR"), closeTo(10.1, 0.001));
     }
 
 }
