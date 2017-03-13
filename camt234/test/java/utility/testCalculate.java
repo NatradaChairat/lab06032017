decf4dd39593ea5a118e1fd07e12c62b46e05a1f
DESKTOP-9CKSS1Q\CAMT <natrada_chairat@cmu.ac.th>
2017-03-13 18:07:50 +0700

    create testCalculate class

---
+++ camt234/test/java/utility/testCalculate.java
@@ -0,0 +1,19 @@
+package utility;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class GradeCalculatorTest {
+    @Test
+    public void testCalculate(){
+        GradeCalculator gradeCalculator=new GradeCalculator();
+        assertThat(gradeCalculator.calculate(25,28,32),is('B'));
+        assertThat(gradeCalculator.calculate(25,25,15),is('C'));
+        assertThat(gradeCalculator.calculate(25,10,05),is('F'));
+        assertThat(gradeCalculator.calculate(30,40,30),is('A'));
+        assertThat(gradeCalculator.calculate(30,40,-30),is('U'));
+        assertThat(gradeCalculator.calculate(-20,40,30),is('U'));
+
+    }
+}
